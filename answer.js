9c0b71fd3283c4f291566f9cf2c6699e94afe156
Asmual Obaidul Hoque <73558650+Asmual@users.noreply.github.com>
2026-09-01 22:52:55 +0600

    Solve Question 4: Dhaka CNG Fare Meter with test cases

--- answer.js
+++ answer.js
@@ -64,3 +64,31 @@ function validateUsername(username) {
 // console.log(validateUsername("superadmin99"));// "Reserved Word"
 // console.log(validateUsername("Admin_Rahim")); // "Reserved Word"
 
+// Question 4: Dhaka CNG Fare Meter
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+    let fare = 0;
+
+    if (distance <= 2) {
+        fare += 50;
+    } else {
+        fare += 50 + (distance - 2) * 15;
+    }
+
+    fare += waitingMinutes * 2;
+
+    if (isNight) {
+        fare += fare * 0.20;
+    }
+
+    return fare;
+}
+
+// Test cases:
+// console.log(getCngFare(2));                // 50
+// console.log(getCngFare(1));                // 50
+// console.log(getCngFare(5));                // 95
+// console.log(getCngFare(10));               // 170
+// console.log(getCngFare(5, false, 10));     // 115
+// console.log(getCngFare(5, true));          // 114
+// console.log(getCngFare(5, true, 10));      // 138
+
